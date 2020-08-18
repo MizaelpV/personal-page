fb3050eb9b70ee77dcb85463eaeba5981a90d9c1
Mizael Paredes <mizael.paredes@externos.bci.cl>
2020-08-18 00:52:15 -0400

    creacion de version desktop personal-page construction  page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import frontPage from './assets/frontPagePortfolio.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 import './App.css';
 
-function App() {
+const App = () => {
 	return (
-		<div className='App'>
-			<h3>Testing</h3>
-		</div>
+		<>
+			<div className='App'>
+				<div className='container-text'>
+					<h1>Hello, I´m building this site to show my work</h1>
+					<p>
+						Stay tune please{' '}
+						<span role='img' aria-label='fire'>
+							🔥
+						</span>
+					</p>
+				</div>
+				<img src={frontPage} alt='Front page' />
+			</div>
+			<footer>
+				<div className='social-media'>
+					<a
+						href='https://instagram.com/mizaelpvielma'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<FontAwesomeIcon icon={faInstagram} />
+					</a>
+					<a
+						href='https://github.com/mizaelpv'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<FontAwesomeIcon icon={faGithub} />
+					</a>
+					<a
+						href='https://twitter.com/mizaelpvielma'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<FontAwesomeIcon icon={faTwitter} />
+					</a>
+				</div>
+			</footer>
+		</>
 	);
-}
+};
 
 export default App;
