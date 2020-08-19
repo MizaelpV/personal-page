716c499cdbd19023d74fcb0ceabaccb55a222d7a
Mizael Paredes <mizael.paredes@externos.bci.cl>
2020-08-18 20:07:21 -0400

    Updates

---
+++ precache-manifest.f6fe14e2a54212b5931510eece234b26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acae5f922cf51116cd36e236b7395c84",
+    "url": "/MizaelpV/personal-page/index.html"
+  },
+  {
+    "revision": "26866f6a524bb84a7857",
+    "url": "/MizaelpV/personal-page/static/css/main.c2e0b0f5.chunk.css"
+  },
+  {
+    "revision": "7ab51171455abdcf2596",
+    "url": "/MizaelpV/personal-page/static/js/2.11305c4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MizaelpV/personal-page/static/js/2.11305c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26866f6a524bb84a7857",
+    "url": "/MizaelpV/personal-page/static/js/main.e982a071.chunk.js"
+  },
+  {
+    "revision": "1b414a6c5f87c744cdb0",
+    "url": "/MizaelpV/personal-page/static/js/runtime-main.ec722928.js"
+  },
+  {
+    "revision": "c0c6e550ca15888000c4886715a1e7c5",
+    "url": "/MizaelpV/personal-page/static/media/frontPagePortfolio.c0c6e550.svg"
+  }
+]);
